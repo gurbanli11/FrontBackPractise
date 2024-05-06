c31c19b69e88a50178a8a3ad6a32519ed87f656c
safarovamehriban <smeh04@mail.ru>
2024-05-06 13:42:18 +0400

    create

---
+++ WebApplicationFirst/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ContactUs() 
+        {
+            return View();
+        }
+    }
+}
